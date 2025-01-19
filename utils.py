a82254b39afd8b04625ef5f4ec2df3d0e2484369
iamvbenz49 <benzwrites@gmail.com>
2025-01-19 22:08:27 +0530

    check in

--- utils.py
+++ utils.py
@@ -6,34 +6,31 @@ from pymilvus import MilvusClient
 url = os.getenv("URI")
 embedding_url = os.getenv("EMBEDDING_URL")
 hf_token = os.getenv("HF_TOKEN")
-client = MilvusClient(uri=url)
+# client = MilvusClient(uri=url)
+model_id = "sentence-transformers/all-MiniLM-L6-v2"
 
 
-def embedding_fn(queries):   
-    headers = {
-        "Authorization": f"Bearer {hf_token}",
-        "Content-Type": "application/json",
-    }
 
-    payload = '{"inputs": "Deploying my first endpoint was an amazing experience."}'
-    response = requests.post(embedding_url, json=payload, headers=headers)
+def embedding_fn(texts):   
+    api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+    headers = {"Authorization": f"Bearer {hf_token}"}
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return f"Error: {response.status_code}, {response.text}"
+    response = requests.post(api_url, headers=headers, json={"inputs": texts, "options":{"wait_for_model":True}})
+    return response.json()
+
 
 
 def fetch_documents(query, collection_name = "demo_collection"):
-    query_vectors = embedding_fn([query])
-    res = client.search(
-        collection_name=collection_name, 
-        data=query_vectors,  
-        limit=2, 
-        output_fields=["text"],
-    )
-    docs = [doc["entity"]["text"] for doc in res[0]]
-    return docs
+    # query_vectors = embedding_fn([query])
+    # res = client.search(
+    #     collection_name=collection_name, 
+    #     data=query_vectors,  
+    #     limit=2, 
+    #     output_fields=["text"],
+    # )
+    # docs = [doc["entity"]["text"] for doc in res[0]]
+    # return docs
+    return ["hello bros"]
 
 
 
@@ -42,5 +39,5 @@ def fetch_documents(query, collection_name = "demo_collection"):
 if __name__ == '__main__':
     queries = ["Who is Alan Turing?", "What is AI?"]
     query_vectors = embedding_fn(queries)
-
+    print(hf_token)
     print(query_vectors)
